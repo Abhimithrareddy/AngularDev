d014922fa281626c1ae69ff672586bc4282cc727
Ayyappa Konda <ayyappak@a-t-g.com>
2020-02-25 16:49:17 -0600

    ResponseForms3

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -59,14 +60,22 @@ export class RecipeEditComponent implements OnInit {
     });
   }
 
-  get controls() { // a getter!
+  get controls() {
     return ( <FormArray> this.recipeForm.get('ingredients')).controls;
   }
   onSubmit() {
-
+    // const newRecipe = new Recipe(this.recipeForm.value['name'],
+    // this.recipeForm.value['description'],
+    // this.recipeForm.value['imagePath'],
+    // this.recipeForm.value['ingredients']
+    // );
+    if (this.editMode) {
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+    } else {
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
   }
   onAddIngredient() {
-
     (<FormArray>this.recipeForm.get('ingredients')).push(
       new FormGroup({
         'name': new FormControl(null, Validators.required),
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,10 +14,13 @@ export class RecipeListComponent implements OnInit {
   constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.recipeService.recipesChanged
+    .subscribe((recipes: Recipe[]) => {
+      this.recipes = recipes;
+    });
     this.recipes = this.recipeService.getRecipes();
   }
   onNewRecipe() {
-    console.log('hoij');
     this.router.navigate(['new'], {relativeTo: this.route});
   }
 
