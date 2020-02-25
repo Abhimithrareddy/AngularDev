02e828e07bab3abb88cd78471ac870e7bf116232
Ayyappa Konda <ayyappak@a-t-g.com>
2020-02-25 14:21:40 -0600

    reactiveForms1

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -43,7 +43,7 @@ export class RecipeEditComponent implements OnInit {
               'name': new FormControl(ingredient.name, Validators.required),
               'amount': new FormControl(ingredient.amount, [
                 Validators.required,
-                 Validators.pattern(/^[1-9]+[0-9]*$/)
+                 Validators.pattern(/^-?(0|[1-9]\d*)?$/)
               ])
             })
           );
@@ -72,7 +72,7 @@ export class RecipeEditComponent implements OnInit {
         'name': new FormControl(null, Validators.required),
         'amount': new FormControl(null, [
           Validators.required,
-           Validators.pattern(/^[1-9]+[0-9]*$/)
+           Validators.pattern(/^-?(0|[1-9]\d*)?$/)
         ])
       })
     );
